afdd91b96910ce39747ab2b23dea939c0fb46a36
LeoEvergarden <95556309+LiyangQian@users.noreply.github.com>
2024-04-23 21:47:20 +0800

    Add files via upload

---
+++ src/main/java/com/virtualbank/controller/ParentHomeController.java
@@ -0,0 +1,34 @@
+package com.virtualbank.controller;
+
+import com.virtualbank.service.TaskService;
+import com.virtualbank.ui.Page04_ParentHome;
+import com.virtualbank.ui.Window3_CreateNewTask;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ParentHomeController {
+    private Page04_ParentHome parentHomeUI;
+    private Window3_CreateNewTask createTaskWindow;
+    private CreateTaskController createTaskController; // 持久化控制器
+    private TaskService taskService = new TaskService();
+
+    public ParentHomeController(Page04_ParentHome parentHomeUI) {
+        this.parentHomeUI = parentHomeUI;
+        attachEventHandlers();
+    }
+
+    private void attachEventHandlers() {
+        parentHomeUI.getCreateTaskButton().addActionListener(e -> showCreateTaskWindow());
+    }
+
+    private void showCreateTaskWindow() {
+        if (createTaskWindow == null) {
+            createTaskWindow = new Window3_CreateNewTask();
+            createTaskController = new CreateTaskController(taskService, createTaskWindow);
+        }
+        createTaskWindow.setVisible(true);
+        parentHomeUI.setVisible(false);
+    }
+}
+
