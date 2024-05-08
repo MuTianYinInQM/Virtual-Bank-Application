695cc5b4d94e2ce339880b7c8bdb422b9059ea8a
rainbow <2@qq.com>
2024-05-08 19:36:27 +0800

    更改页面1——登陆页面

--- src/main/java/com/virtualbank/ui/Page01_Login.java
+++ src/main/java/com/virtualbank/ui/Page01_Login.java
@@ -42,29 +42,41 @@ public class Page01_Login extends JFrame {
         // 输入框
         Border border = BorderFactory.createLineBorder(new Color(0x3d6de1), 2);
         username_textField = new JTextField();
-        username_textField.setBounds(450, 220, 360, 50);
+        username_textField.setBounds(450, 310, 360, 50);
         Font font = new Font(username_textField.getFont().getName(), Font.PLAIN, 20); // 创建一个新字体对象，设置大小为20
         username_textField.setFont(font);
         username_textField.setBorder(border);
         backgroundLabel.add(username_textField);
 
         password_textField = new JPasswordField();
-        password_textField.setBounds(450, 300, 360, 50);
+        password_textField.setBounds(450, 385, 360, 50);
         password_textField.setFont(font);
         password_textField.setBorder(border);
         backgroundLabel.add(password_textField);
 
+        // 提示注册
+        JLabel registerNote1 = new JLabel("If you haven't had an account,");
+        JLabel registerNote2 = new JLabel("click on \"Register\" below to register one.");
+        registerNote1.setForeground(new Color(0x959aa4));
+        registerNote2.setForeground(new Color(0x959aa4));
+        registerNote1.setBounds(505, 450, 1000, 20);
+        registerNote2.setBounds(465, 468, 1000, 20);
+        registerNote1.setFont(new Font(registerNote1.getFont().getName(), Font.PLAIN, 17));
+        registerNote2.setFont(new Font(registerNote1.getFont().getName(), Font.PLAIN, 17));
+        backgroundLabel.add(registerNote1);
+        backgroundLabel.add(registerNote2);
+
         // 确认按钮
         confirmButton = new JButton();
-        confirmButton.setBounds(530, 375, 200, 55);
+        confirmButton.setBounds(440, 505, 200, 55);
         ImageIcon confirmButtonIcon = new ImageIcon("images/ConfirmButton.png");
         confirmButton.setIcon(confirmButtonIcon);
         confirmButton.setBorder(null);
         backgroundLabel.add(confirmButton);
 
         // 注册按钮
-        registerButton = new JButton("Register");
-        registerButton.setBounds(580, 435, 122, 44);
+        registerButton = new JButton();
+        registerButton.setBounds(630, 505, 200, 55);
         ImageIcon registerButtonIcon = new ImageIcon("images/RegisterButton.png");
         registerButton.setIcon(registerButtonIcon);
         registerButton.setBorder(null);
