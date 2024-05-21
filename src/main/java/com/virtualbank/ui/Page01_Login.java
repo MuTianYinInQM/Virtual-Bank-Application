5d0add865907f6bf9079ce3da96f48ca0c3802b6
rainbow <2@qq.com>
2024-05-21 17:05:28 +0800

    Page01_Login.java javadoc

--- src/main/java/com/virtualbank/ui/Page01_Login.java
+++ src/main/java/com/virtualbank/ui/Page01_Login.java
@@ -1,32 +1,74 @@
+/**
+ * The first UI page
+ * @author Yunbo Jia
+ * @version 2.0 2024-5-21
+ */
 package com.virtualbank.ui;
 
 import java.awt.*;
 import javax.swing.*;
 import javax.swing.border.Border;
 
+/**
+ * Page01_Login is the first UI page for users to either log in or register, a subclass of JFrame.
+ * This UI page includes text fields for username and password
+ * as well as a confirm button and a register button.
+ *
+ *
+ * @see JFrame
+ */
 public class Page01_Login extends JFrame {
 
+    /** Username input field */
     private JTextField username_textField; // 用户名输入框
+    /** Password input field */
     private JPasswordField password_textField;
+    /** Confirm button */
     private JButton confirmButton;
+    /** Register button */
     private JButton registerButton;
 
+    /**
+     * Gets the username input field.
+     *
+     * @return the JTextField object for the username input field
+     */
     public JTextField getUsername_textField() {
         return username_textField;
     }
 
+    /**
+     * Gets the password input field.
+     *
+     * @return the JPasswordField object for the password input field
+     */
     public JPasswordField getPassword_textField() {
         return password_textField;
     }
 
+    /**
+     * Gets the confirm button.
+     *
+     * @return the JButton object for the confirm button
+     */
     public JButton getConfirmButton() {
         return confirmButton;
     }
 
+    /**
+     * Gets the register button.
+     *
+     * @return the JButton object for the register button
+     */
     public JButton getRegisterButton() {
         return registerButton;
     }
 
+    /**
+     * Constructs a new Page01_Login frame.
+     * Sets up the frame size, title, layout, initializes components,
+     * and adds them to the frame.
+     */
     public Page01_Login() {
         final int window_width = 1260;
         final int window_height = 780;
@@ -39,7 +81,7 @@ public class Page01_Login extends JFrame {
         backgroundLabel.setBounds(0, 0, window_width, window_height);
         getContentPane().add(backgroundLabel);
 
-        // 输入框
+        // text fields
         Border border = BorderFactory.createLineBorder(new Color(0x3d6de1), 2);
         username_textField = new JTextField();
         username_textField.setBounds(450, 310, 360, 50);
@@ -54,7 +96,6 @@ public class Page01_Login extends JFrame {
         password_textField.setBorder(border);
         backgroundLabel.add(password_textField);
 
-        // 提示注册
         JLabel registerNote1 = new JLabel("If you haven't had an account,");
         JLabel registerNote2 = new JLabel("click on \"Register\" below to register one.");
         registerNote1.setForeground(new Color(0x959aa4));
@@ -66,7 +107,7 @@ public class Page01_Login extends JFrame {
         backgroundLabel.add(registerNote1);
         backgroundLabel.add(registerNote2);
 
-        // 确认按钮
+        // confirm button
         confirmButton = new JButton();
         confirmButton.setBounds(440, 505, 200, 55);
         ImageIcon confirmButtonIcon = new ImageIcon("images/ConfirmButton.png");
@@ -75,7 +116,7 @@ public class Page01_Login extends JFrame {
         confirmButton.setContentAreaFilled(false);
         backgroundLabel.add(confirmButton);
 
-        // 注册按钮
+        // register button
         registerButton = new JButton();
         registerButton.setBounds(630, 505, 200, 55);
         ImageIcon registerButtonIcon = new ImageIcon("images/RegisterButton.png");
@@ -89,6 +130,11 @@ public class Page01_Login extends JFrame {
         setVisible(true);
     }
 
+    /**
+     * Creates and displays the Page01_Login frame.
+     *
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
         Page01_Login page01Login = new Page01_Login();
     }
