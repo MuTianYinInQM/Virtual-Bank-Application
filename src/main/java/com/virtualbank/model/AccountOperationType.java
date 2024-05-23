1cc36b57431ca1ac55d10440978cc82765ef2521
tianmu <yinmutian@bupt.edu.cn>
2024-05-23 12:56:00 +0800

    change the operation type to English

--- src/main/java/com/virtualbank/model/AccountOperationType.java
+++ src/main/java/com/virtualbank/model/AccountOperationType.java
@@ -1,15 +1,15 @@
 package com.virtualbank.model;
 
 public enum AccountOperationType {
-    CONSUME("CONSUMPTION", "Spending money, can only be spent from the piggy bank, equivalent to money disappearing into the external real world from the virtual software"),
-    SAVE("DEPOSIT", "Depositing money, can only deposit money into the piggy bank, equivalent to money magically increasing, from the external real world into the virtual software"),
-    INITIAL_SAVE("INITIAL_DEPOSIT", "Initial deposit amount when creating a savings account, by default it is taken from the piggy bank"),
-    TRANSFER("TRANSFER", "Transfer between two UUID cards, requires compliance checks"),
-    TRANSFER_FROM("TRANSFER_FROM", "Transfer between two UUID cards, compliance check required, from is the provider of the money"),
-    TRANSFER_TO("TRANSFER_TO", "Transfer between two UUID cards, compliance check required, to is the receiver of the money"),
+    CONSUME("CONSUMPTION", "Spending money"),
+    SAVE("DEPOSIT", "Depositing money"),
+    INITIAL_SAVE("INITIAL_DEPOSIT", "Initial deposit amount when creating a savings account"),
+    TRANSFER("TRANSFER", "Transfer between two cards"),
+    TRANSFER_FROM("TRANSFER_FROM", "Transfer from another card"),
+    TRANSFER_TO("TRANSFER_TO", "Transfer to another card"),
 
-    INTEREST("INTEREST_CALCULATION", "Calculate the interest rate for a specific account, need to specify UUID"),
-    PRIZE("REWARD", "A special way of depositing money, represents money awarded to the user");
+    INTEREST("INTEREST_CALCULATION", "Calculate the interest rate"),
+    PRIZE("REWARD", "Get reward from tasks"),;
 
     private final String description;
     private final String name;
